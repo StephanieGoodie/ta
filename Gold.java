285be801e504e3f7af6067086ac45f639f5e4c87
StephanieGoodie <122505716+StephanieGoodie@users.noreply.github.com>
2023-01-29 13:51:25 -0500

    Delete Gold.java

--- Gold.java
+++
@@ -1,9 +0,0 @@
-public class Gold extends Item{
-
-    private int amt;
-
-    public Gold(int amt){
-        super("Gold", String.format("A round coin with %s stamped on the front.", (amt)), amt);
-        this.amt = amt;
-    }
-}
