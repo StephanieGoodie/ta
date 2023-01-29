b7e6846d57d35ccc90fe0e6165da9226ea38a69e
StephanieGoodie <122505716+StephanieGoodie@users.noreply.github.com>
2023-01-29 13:42:15 -0500

    Add files via upload
    Main program

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Gold goldBar = new Gold(10);
+        Sword magicSword = new Sword();
+        Mace bigMace = new Mace("Mace", "Large Mace weapon", 12, 35);
+        Pillow softPillow = new Pillow();
+
+    }
+}
