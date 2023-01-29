52e54e7b0001ab481a35decc6fd56196ff5a3b0d
StephanieGoodie <122505716+StephanieGoodie@users.noreply.github.com>
2023-01-29 13:51:42 -0500

    Delete Mace.java

--- Mace.java
+++
@@ -1,6 +0,0 @@
-public class Mace extends Weapon{
-
-    public Mace(String name, String desc, int value, int damage) {
-        super("Mace", "A mace somewhat more dangerous than a sword", 10, 30);
-    }
-}
